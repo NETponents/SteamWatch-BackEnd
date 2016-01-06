a26d40bf9e56e6e2fd6e845e34a38680a1ae261a
ARMmaster17 <wordman05@gmail.com>
2016-01-06 17:23:11 -0500

    Work on static pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/faq', function(req, res, next) {
+    res.render('faq', {title: 'FAQ'});
+});
+router.get('/download', function(req, res, next) {
+    res.render('download', {title: 'Download'});
+});
+router.get('/contact', function(req, res, next) {
+    res.render('contact', {title: 'Contact'});
 });
 
 module.exports = router;
