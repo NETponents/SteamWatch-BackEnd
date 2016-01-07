f1dba1b2717d334c3bc3c1e2c538859623dac5a9
ARMmaster17 <wordman05@gmail.com>
2016-01-07 17:05:49 -0500

    More work on API

--- routes/API/v1.js
+++ routes/API/v1.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var requestify = require('requestify');
 
 router.get('/version', function(req, res, next) {
     res.json({ version: "1.0.0" });
@@ -10,7 +11,15 @@ router.get('/sw/news', function(req, res, next) {
 });
 
 router.get('/player/info/:uid', function(req, res, next) {
-  res.send(req.params.uid);
+  var resp;
+  //requestify.get("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2?key=X&steamids=" + req.params.uid)
+  requestify.get('https://api.steampowered.com/ISteamNews/GetNewsForApp/v2?appid=2000')
+    .then(function(response) {
+      // Get the response body (JSON parsed or jQuery object for XMLs)
+      resp = response.getBody();
+      res.json(resp);
+    }
+  );
 });
 
 module.exports = router;
